b1110dc0a91ebbe55884e602277baf740ca72e49
Sweta <shwetas383@gmail.com>
2020-05-10 10:01:03 +0530

    currency service basic

---
+++ CurrencyConversion/src/main/java/com/micro/Currency/CurrencyConversion/CurrencyConversionController.java
@@ -0,0 +1,21 @@
+package com.micro.Currency.CurrencyConversion;
+
+import java.math.BigDecimal;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication  
+@RestController
+public class CurrencyConversionController 
+
+{
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")       //where {from} and {to} are path variable  
+	public CurrencyConersionValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to)  //from map to USD and to map to INR  
+	{     
+	return new  CurrencyConersionValue(1000L, from, to, BigDecimal.valueOf(65));  
+	}  
+	
+}
