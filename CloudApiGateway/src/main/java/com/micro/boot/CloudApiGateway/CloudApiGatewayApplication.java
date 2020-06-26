9a67be64956e280c485327c6cc7fff0335fe381e
w2cluster342@iiht.tech <w2cluster342@iiht.tech>
2020-06-26 20:49:36 +0530

    cld

--- CloudApiGateway/src/main/java/com/micro/boot/CloudApiGateway/CloudApiGatewayApplication.java
+++ CloudApiGateway/src/main/java/com/micro/boot/CloudApiGateway/CloudApiGatewayApplication.java
@@ -3,9 +3,11 @@ package com.micro.boot.CloudApiGateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 @EnableEurekaClient
+@Component
 public class CloudApiGatewayApplication {
 
 	public static void main(String[] args) {
