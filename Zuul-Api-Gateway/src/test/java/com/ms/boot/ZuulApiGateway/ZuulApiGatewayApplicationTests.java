6133b1827b3aebb29129479ccbdac09cb6bccb13
w2cluster342@iiht.tech <w2cluster342@iiht.tech>
2020-06-28 03:36:06 +0530

    zuul completed

---
+++ Zuul-Api-Gateway/src/test/java/com/ms/boot/ZuulApiGateway/ZuulApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.boot.ZuulApiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
