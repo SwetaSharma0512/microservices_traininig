ee338ecd9939ea5e70961cd4df82725f4cef00c8
w2cluster342@iiht.tech <w2cluster342@iiht.tech>
2020-06-27 20:45:46 +0530

    cld

--- CloudApiGateway/src/main/java/com/micro/boot/CloudApiGateway/CloudApiGatewayApplication.java
+++ CloudApiGateway/src/main/java/com/micro/boot/CloudApiGateway/CloudApiGatewayApplication.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 @EnableEurekaClient
-@Component
+
 public class CloudApiGatewayApplication {
 
 	public static void main(String[] args) {
