9202e5a79b05aed4f0d2ff2e8835cf1564906055
w2cluster342@iiht.tech <w2cluster342@iiht.tech>
2020-07-10 17:42:35 +0530

    dockerrunfil\e

--- CurrencyConversion/src/main/java/com/micro/Currency/CurrencyConversion/CurrencyConversionApplication.java
+++ CurrencyConversion/src/main/java/com/micro/Currency/CurrencyConversion/CurrencyConversionApplication.java
@@ -1,13 +1,21 @@
 package com.micro.Currency.CurrencyConversion;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan("com.micro.Currency.CurrencyConversion")
 public class CurrencyConversionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyConversionApplication.class, args);
+		
+
+		System.out.println("MS1 running..... here");
 	}
 
 }
