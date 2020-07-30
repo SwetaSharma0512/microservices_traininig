6b8dc102dd6ebfa7b71b22326b75774ade67e2fa
w2cluster342@iiht.tech <w2cluster342@iiht.tech>
2020-07-30 13:53:06 +0530

    dockeriset till zull

--- CurrencyConversion/src/main/java/com/micro/Currency/CurrencyConversion/CurrencyConversionApplication.java
+++ CurrencyConversion/src/main/java/com/micro/Currency/CurrencyConversion/CurrencyConversionApplication.java
@@ -1,10 +1,14 @@
 package com.micro.Currency.CurrencyConversion;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
-
+@EnableAutoConfiguration
+@Configuration
 @SpringBootApplication
+
 public class CurrencyConversionApplication {
 
 	public static void main(String[] args) {
--- CurrencyConversion/src/main/java/com/micro/Currency/CurrencyConversion/CurrencyConversionController.java
+++ CurrencyConversion/src/main/java/com/micro/Currency/CurrencyConversion/CurrencyConversionController.java
@@ -1,6 +1,7 @@
 package com.micro.Currency.CurrencyConversion;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@SpringBootApplication
 
 public class CurrencyConversionController {
 	@Autowired  
